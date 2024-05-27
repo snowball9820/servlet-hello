ce1cb95c70175fab9131eaf59c1ef1913f9f769a
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-05-28 05:12:18 +0900

    Feat: Add ServletComponentScan for automatic servlet registration
    - @ServletComponentScan을 추가하여 스프링 부트 애플리케이션에서 현재 패키지 및 하위 패키지에 있는 서블릿을 자동으로 찾고 등록하도록 함.

--- src/main/java/hi/servlet/ServletApplication.java
+++ src/main/java/hi/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hi.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //spring이 자동으로 현재 내 패키지+하위 패키지 Servlet을 찾고 자동으로 등록, 실행
 @SpringBootApplication
 public class ServletApplication {
 
