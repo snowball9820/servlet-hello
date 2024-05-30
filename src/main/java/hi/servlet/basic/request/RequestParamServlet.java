27d512efedb9a69495494c874b854cbaf0620e50
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-05-30 23:25:50 +0900

    Docs: add description

--- src/main/java/hi/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hi/servlet/basic/request/RequestParamServlet.java
@@ -36,7 +36,7 @@ public class RequestParamServlet extends HttpServlet {
         System.out.println("age = " + age);
         System.out.println();
 
-        System.out.println("[이름이 같은 복수 파라미터 조회");
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
         String[] usernames = request.getParameterValues("username");
         for (String name : usernames) {
             System.out.println("username = " + name);
